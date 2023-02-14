3de5e0f6af8667d31b9773db281c17fe75de89a1
Hidran <hidran.arias@spryker.com>
2023-02-15 00:52:31 +0100

    Adds state machine

--- config/Shared/common/config_oms-development.php
+++ config/Shared/common/config_oms-development.php
@@ -39,15 +39,16 @@ $config[NopaymentConstants::WHITELIST_PAYMENT_METHODS] = [
 
 $config[OmsConstants::ACTIVE_PROCESSES] = array_merge([
     'Nopayment01',
+    'Demo01',
     'DummyPayment01',
 ], $config[OmsConstants::ACTIVE_PROCESSES]);
 
 $config[SalesConstants::PAYMENT_METHOD_STATEMACHINE_MAPPING] = array_replace(
     $config[SalesConstants::PAYMENT_METHOD_STATEMACHINE_MAPPING],
     [
-    DummyPaymentConfig::PAYMENT_METHOD_INVOICE => 'DummyPayment01',
-    DummyPaymentConfig::PAYMENT_METHOD_CREDIT_CARD => 'DummyPayment01',
-    NopaymentConfig::PAYMENT_PROVIDER_NAME => 'Nopayment01',
-    GiftCardConfig::PROVIDER_NAME => 'DummyPayment01',
+        DummyPaymentConfig::PAYMENT_METHOD_INVOICE => 'Demo01',
+        DummyPaymentConfig::PAYMENT_METHOD_CREDIT_CARD => 'DummyPayment01',
+        NopaymentConfig::PAYMENT_PROVIDER_NAME => 'Nopayment01',
+        GiftCardConfig::PROVIDER_NAME => 'DummyPayment01',
     ],
 );
